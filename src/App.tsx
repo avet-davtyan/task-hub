bb49888914936ef74cd013f6ed341622cc2d2ecb
avet-davtyan <avetdavtyan04@gmail.com>
2023-09-07 10:58:22 +0400

    creating boards by configs

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 import TodoBoard from "./components/todoBoard/TodoBoard";
 import {Stack} from '@mui/material';
+// @ts-ignore
+import {DragDropContext} from 'react-beautiful-dnd';
+
+const boardConfigs = [
+    {
+        // boardStyle: {position: 'absolute'},
+        tasks: [
+            {task_id: 1, name: 'first board 1'},
+            {task_id: 2, name: 'first board 2'}
+        ],
+        droppableId: '1'
+    },
+    {
+        boardStyle: {},
+        tasks: [
+            {task_id: 10, name: 'second board 5'},
+            {task_id: 20, name: 'second board 7'}
+        ],
+        droppableId: '2'
+    },
+    {
+        boardStyle: {position: 'absolute', right: '0px'},
+        tasks: [
+            {task_id: 100, name: 'second board 5'},
+            {task_id: 200, name: 'second board 7'},
+            {task_id: 300, name: 'ssssssss'}
+        ],
+        droppableId: '3'
+    }
+]
 
 function App() {
     return (
-        <Stack style={{
-            backgroundColor: '#DCDCDC',
-            width: '100%',
-            height: '100%',
-            position: 'absolute'
-        }} justifyContent='center' alignItems='center'>
-            <TodoBoard boardStyle={{
 
-                // transform: 'translate(-50%, -50%)',
+        <Stack direction='row'>
+            <DragDropContext>
+                {boardConfigs.map(boardConfig => (
+                    <TodoBoard key={boardConfig.droppableId}
+                               boardStyle={boardConfig?.boardStyle}
+                               initialTasks={boardConfig?.tasks}
+                               droppableId={boardConfig?.droppableId}
 
-
-            }}/>
+                    />
+                ))}
+            </DragDropContext>
         </Stack>
     );
 }
--- src/components/todoBoard/TodoBoard.tsx
+++ src/components/todoBoard/TodoBoard.tsx
@@ -19,16 +19,16 @@ interface task {
 
 
 const TodoBoard = ({
-                       boardStyle = {} as BoardStyle,
+                       boardStyle = {} as any,
                        TaskRenderer = CustomTaskRenderer,
                        AddButton = CustomAddButton,
                        TaskTextField = CustomTextField,
+                       droppableId = 'droppable',
+                       initialTasks = [{task_id: 1, name: 'task'}]
                    }) => {
 
-    const [tasks, setTasks] = useState<any[]>([
-        {name: 'task #1', task_id: 1},
-        {name: 'task #2', task_id: 2},
-    ])
+    const [tasks, setTasks] = useState<any[]>(initialTasks)
+
     const [text, setText] = useState<string>('');
     const [showDelEdit, setShowDelEdit] = useState<boolean>(false);
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -36,27 +36,25 @@ const TodoBoard = ({
     }
     const addTask = () => {
         if (text) setTasks([...tasks, {name: text, task_id: Math.random()}]);
+        console.log('add')
+        setText('');
     }
     const handleDelete = (task: any) => {
         setTasks(tasks.filter(_task => _task.task_id !== task.task_id));
     }
 
     const grid = 8;
-    const getItemStyle = (isDragging: any, draggableStyle: any) => ({
-        ...draggableStyle,
-        userSelect: 'none',
-        // position: '',
-        height: '100px',
-        backgroundColor: 'blue',
+    const handleDragDrop = (results: any) => {
+        if (!results.destination) return;
+        const items = Array.from(tasks);
+        const [reorderedItem] = items.splice(results.source.index, 1);
+        items.splice(results.destination.index, 0, reorderedItem);
+        setTasks(items);
+    }
 
-        margin: `0 0 8px 0`,
-    });
-
-    const getListStyle = (isDraggingOver: any) => ({
-        height: '400px',
-    });
 
     return (
+
         <Card sx={{
             width: '400px',
             padding: '30px 20px 30px 20px',
@@ -70,40 +68,41 @@ const TodoBoard = ({
                     <TaskTextField handleChange={handleChange}/>
                     <AddButton handleAddTask={addTask}/>
                 </Stack>
-                <Divider/>
-                <DragDropContext>
-                    <Droppable droppableId="droppable">
-                        {(provided: any, snapshot: any) => (
-                            <Stack
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                                spacing={1}
-                            >
-                                {tasks.map((task, index) => (
-                                    <Draggable key={task.task_id} draggableId={task.task_id.toString()} index={index}>
-                                        {(provided: any, snapshot: any) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                            >
-                                                <Stack sx={{
-                                                    height: '50px',
-                                                    position: 'relative'
-                                                }}>
-                                                    <TaskRenderer task={task} handleDelete={handleDelete}/>
-                                                </Stack>
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                                {provided.placeholder}
-                            </Stack>
-                        )}
-                    </Droppable>
-                </DragDropContext>
+
+
+                <Droppable droppableId={droppableId}>
+                    {(provided: any, snapshot: any) => (
+                        <Stack
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+
+                        >
+                            {tasks.map((task, index) => (
+                                <Draggable key={task.task_id} draggableId={task.task_id.toString()} index={index}>
+                                    {(provided: any, snapshot: any) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                        >
+                                            <Stack sx={{
+                                                height: '50px',
+                                                position: 'relative',
+
+                                            }}>
+                                                <TaskRenderer task={task} handleDelete={handleDelete}/>
+                                            </Stack>
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </Stack>
+                    )}
+                </Droppable>
             </Stack>
         </Card>
+
     )
 }
 
