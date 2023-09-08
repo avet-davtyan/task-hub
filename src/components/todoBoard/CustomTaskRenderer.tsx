72fe99f185ce010c5d0a06a14e08010c4148d63f
avet-davtyan <avetdavtyan04@gmail.com>
2023-09-08 18:19:01 +0400

    add edit dialog for default task update

--- src/components/todoBoard/CustomTaskRenderer.tsx
+++ src/components/todoBoard/CustomTaskRenderer.tsx
@@ -6,52 +6,96 @@ import {CSSObject} from '@emotion/react';
 import {useTheme} from "@mui/material";
 import {task} from "../types/types";
 
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 const CustomTaskRenderer = ({task, handleDelete, handleUpdate}: {
     task: task, handleDelete: (taskToDelete: task) => void,
     handleUpdate: (taskToUpdate: task, updatedTask: task) => void
 }) => {
     const theme = useTheme();
-    const [showEditDel, setShowEditDel] = useState<boolean>(false);
+    const [openDialog, setOpenDialog] = useState<boolean>(false);
+
+    const [textName, setTextName] = useState<string>(task?.name);
+
+    const handleClose = () => {
+        setOpenDialog(false);
+    }
+
+    const handleOpen = () => {
+        setOpenDialog(true);
+    }
 
     return (
-        <Card sx={{
-            height: '50px',
-            position: 'relative',
-            m: 0.3,
-            backgroundColor: '#FCFCFC',
-        }}
-              variant='outlined'
-        >
-            <Stack sx={{
-                width: '100%',
-                height: '100%',
-                paddingLeft: '10px'
-            }} justifyContent='center'>
-                <Typography color='text.secondary'>
-                    {task?.name}
-                </Typography>
-
-                <Stack sx={{
-                    position: 'absolute',
-                    right: '0px',
-                    width: '100px',
-                    height: '100%',
-
-                }} direction='row'>
-                    <Button style={{width: '50%', minWidth: 0}} onClick={() => {
-                        handleUpdate(task, {...task, name: 'updated'})
-                    }}>
-                        <EditIcon style={{color: theme.palette.primary.main}}/>
-                    </Button>
-                    <Button style={{width: '50%', minWidth: 0}}
-                            onClick={() => {
-                                handleDelete(task);
-                            }}>
-                        <DeleteIcon style={{color: theme.palette.error.main}}/>
-                    </Button>
+        <>
+            <Dialog open={openDialog} onClose={handleClose}>
+                <Stack sx={{width: '400px'}}>
+                    <DialogTitle>Edit</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            fullWidth
+                            variant="standard"
+                            value={textName}
+                            onChange={(e) => {
+                                setTextName(e.target.value);
+                            }}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={() => {
+                            handleClose();
+                            handleUpdate(task, {...task, name: textName});
+                        }}>Edit</Button>
+                    </DialogActions>
                 </Stack>
-            </Stack>
-        </Card>
+            </Dialog>
+
+            <Card sx={{
+                height: '50px',
+                position: 'relative',
+                m: 0.3,
+                backgroundColor: '#FCFCFC',
+            }}
+                  variant='outlined'
+            >
+                <Stack sx={{
+                    width: '100%',
+                    height: '100%',
+                    paddingLeft: '10px'
+                }} justifyContent='center'>
+                    <Typography color='text.secondary'>
+                        {task?.name}
+                    </Typography>
+
+                    <Stack sx={{
+                        position: 'absolute',
+                        right: '0px',
+                        width: '100px',
+                        height: '100%',
+
+                    }} direction='row'>
+                        <Button style={{width: '50%', minWidth: 0}} onClick={() => {
+                            handleOpen();
+                        }}>
+                            <EditIcon style={{color: theme.palette.primary.main}}/>
+                        </Button>
+                        <Button style={{width: '50%', minWidth: 0}}
+                                onClick={() => {
+                                    handleDelete(task);
+                                }}>
+                            <DeleteIcon style={{color: theme.palette.error.main}}/>
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Card>
+        </>
     )
 }
 
